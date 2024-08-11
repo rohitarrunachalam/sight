46f5351d5a8915c18fae16a0580d06f1d9e8c326
rohitarrunachalam <rrarohit@gmail.com>
2024-08-11 10:35:08 +0530

    adding api files

--- api/test.py
+++ api/test.py
@@ -2,14 +2,14 @@ from langchain_community.agent_toolkits import create_sql_agent
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
 
-db = SQLDatabase.from_uri('postgresql+psycopg2://postgres:210282@localhost/police_hackathon')
+db = SQLDatabase.from_uri('POSTGRES_URI')
 
 print(db.dialect)
 print(db.get_usable_table_names())
 
 
 llm = ChatOpenAI(
-    openai_api_key="sk-proj-gTpKWnovm3XQbrt599AIT3BlbkFJvcXoftWH8YsPZcQYHtn5",
+    openai_api_key="OPENAI_KEY",
     model="gpt-3.5-turbo", temperature=0)
 agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
 agent_executor.invoke(
